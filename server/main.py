8d22e151c79ed9e7380709d140dd917be1fbdeb1
Asus-AYA <janars.2005@mail.ru>
2024-03-26 21:16:42 +0300

    add cors to server

--- server/main.py
+++ server/main.py
@@ -1,9 +1,9 @@
 from flask import Flask
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-if __name__ == '__main__':
-    app.run()
+CORS(app)
+
+@app.route("/")
+def helloworld():
+  return "Hello, cross-origin-world!"
