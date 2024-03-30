f9afd8e025fa24ed86b85015a6a0af51ba6acb97
Asus-AYA <janars.2005@mail.ru>
2024-03-30 19:30:17 +0300

    adding database of users

--- server/main.py
+++ server/main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from for_bd_connect import checking_for_authorized_user
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def helloworld():
-  return "Hello, cross-origin-world!"
+@app.route("/auth", methods=['POST'])
+def authorization():
+  data = request.get_json()
+  username = data['username']
+  password = data['password']
+  if checking_for_authorized_user(username, password) == -1:
+    return 'invalid data'
+  else:
+    return 'success'
+
 
 if __name__ == "__main__":
   app.run()
