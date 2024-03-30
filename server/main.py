fc86b86804d985311eadeae312d5e3f78cc66ee3
Asus-AYA <janars.2005@mail.ru>
2024-03-30 19:49:20 +0300

    fix authresult_Error

--- server/main.py
+++ server/main.py
@@ -1,6 +1,10 @@
+import json
+
 from flask import Flask, request
 from flask_cors import CORS
 from for_bd_connect import checking_for_authorized_user
+from json import dumps
+
 
 app = Flask(__name__)
 CORS(app)
@@ -10,10 +14,13 @@ def authorization():
   data = request.get_json()
   username = data['username']
   password = data['password']
+  res = {"result": None}
   if checking_for_authorized_user(username, password) == -1:
-    return 'invalid data'
+    res["result"] = 'invalid data'
   else:
-    return 'success'
+    res["result"] = 'success'
+  return dumps(res)
+
 
 
 if __name__ == "__main__":
