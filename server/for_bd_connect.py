bb37c50ce2dfc4e48e3308b5d08cc83bcb452694
Asus-AYA <janars.2005@mail.ru>
2024-03-31 17:05:55 +0300

    adding db_config

--- server/for_bd_connect.py
+++ server/for_bd_connect.py
@@ -1,9 +1,15 @@
 import psycopg2
+import configparser
 
-
-def checking_for_authorized_user(username, password):
-    conn = psycopg2.connect(dbname='for_autorisation', user='Admin',
-                            password='1234', host='localhost')
+def checking_for_authorized_user(username: str, password: str):
+    config = configparser.ConfigParser()
+    config.read('for_bd.cfg')
+    data_base_name = config.get('Settings_of_bd', 'dbname')
+    username_of_database = config.get('Settings_of_bd', 'user')
+    password_of_database = config.get('Settings_of_bd', 'password')
+    name_of_host = config.get('Settings_of_bd', 'host')
+    conn = psycopg2.connect(dbname=data_base_name, user=username_of_database,
+                            password=password_of_database, host=name_of_host)
     cursor = conn.cursor()
     cursor.execute('''SELECT id FROM "Autorisation" WHERE username=%s AND password=%s ''', (username, password))
     records = cursor.fetchall()
