299ec13b3a264e82de18b7eb141c58bb5e461ff4
Asus-AYA <janars.2005@mail.ru>
2024-03-31 18:40:05 +0300

    add jwt checking

--- server/for_bd_connect.py
+++ server/for_bd_connect.py
@@ -1,22 +1,34 @@
 import psycopg2
 import configparser
 
-def checking_for_authorized_user(username: str, password: str):
-    config = configparser.ConfigParser()
-    config.read('for_bd.cfg')
-    data_base_name = config.get('Settings_of_bd', 'dbname')
-    username_of_database = config.get('Settings_of_bd', 'user')
-    password_of_database = config.get('Settings_of_bd', 'password')
-    name_of_host = config.get('Settings_of_bd', 'host')
-    conn = psycopg2.connect(dbname=data_base_name, user=username_of_database,
-                            password=password_of_database, host=name_of_host)
-    cursor = conn.cursor()
-    cursor.execute('''SELECT id FROM "Autorisation" WHERE username=%s AND password=%s ''', (username, password))
-    records = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    if len(records) == 0:
-        return -1
-    else:
+class Database_manager:
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('for_bd.cfg')
+        data_base_name = config.get('Settings_of_bd', 'dbname')
+        username_of_database = config.get('Settings_of_bd', 'user')
+        password_of_database = config.get('Settings_of_bd', 'password')
+        name_of_host = config.get('Settings_of_bd', 'host')
+        self.conn = psycopg2.connect(dbname=data_base_name, user=username_of_database,
+                                password=password_of_database, host=name_of_host)
+
+    def __del__(self):
+        self.conn.close()
+
+    def checking_for_authorized_user(self, username: str, password: str):
+        cursor = self.conn.cursor()
+        cursor.execute('''SELECT id FROM "Autorisation" WHERE username=%s AND password=%s ''', (username, password))
+        records = cursor.fetchall()
+        cursor.close()
+        if len(records) == 0:
+            return -1
         return records[0][0]
 
+    def get_username_by_id(self, user_id):
+        cursor = self.conn.cursor()
+        cursor.execute('''SELECT username FROM "Autorisation" WHERE id=%s''', (user_id))
+        records = cursor.fetchall()
+        cursor.close()
+        if len(records) == 0:
+            return -1
+        return records[0][0]
--- server/main.py
+++ server/main.py
@@ -1,25 +1,47 @@
 
 from flask import Flask, request
 from flask_cors import CORS
-from for_bd_connect import checking_for_authorized_user
 from json import dumps
+from for_JWT_generating import ForJWTGenerating
+from for_bd_connect import Database_manager
 
 
+
+database = Database_manager()
 app = Flask(__name__)
 CORS(app)
-
+jwt_generator = ForJWTGenerating()
 @app.route("/auth", methods=['POST'])
 def authorization():
   data = request.get_json()
   username = data['username']
   password = data['password']
-  res = {"result": None}
-  if checking_for_authorized_user(username, password) == -1:
+  res = {"result": None,
+         "jwtToken": None,
+         "username": None}
+  id_of_user = database.checking_for_authorized_user(username, password)
+  if id_of_user == -1:
     res["result"] = 'invalid data'
   else:
+    res["jwtToken"] = jwt_generator.generate_jwt_token(id_of_user)
+    res["username"] = username
     res["result"] = 'success'
   return dumps(res)
 
+@app.route('/user_data', methods=["POST"])
+def returning_of_status_and_username():
+  data = request.get_json()
+  jwt_token = data['jwtToken']
+  res = {"status": None,
+         "username": None}
+  if not jwt_generator.validate_token(jwt_token):
+    res["status"] = 'invalid token'
+    return dumps(res)
+  res["status"] = 'success'
+  user_id = jwt_generator.extract_payload(jwt_token)['userId']
+  username = database.get_username_by_id(user_id)
+  res["username"] = username
+  return dumps(res)
 
 
 if __name__ == "__main__":
