4274cf0eb3a9940d191a03f25a8a69c8c0e9e654
Smoking_Elk <andre12go@mail.ru>
2024-04-06 13:19:42 +0300

    add my profile component logic

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import AdvertisementContainer from '../Advertisement/AdvertisementContainer';
 import Header from '../Header/Header';
 import HeaderContainer from '../Header/HeaderContainer';
 import LoginContainer from '../Login/LoginContainer';
+import MyProfileContainer from '../MyProfile/MyProfileContainer';
 import SignupContainer from '../Signup/SignupContainer';
 import classes from './App.module.css';
 import { Route, Routes } from 'react-router-dom';
@@ -14,6 +15,7 @@ const App = props => {
 			<Routes>
 				<Route path="/login" element={<LoginContainer />} />
 				<Route path="/sign_up" element={<SignupContainer />} />
+				<Route path="/profile" element={<MyProfileContainer />} />
 			</Routes>
 
 			<AdvertisementContainer />
--- client/src/redux/myProfileReducer.js
+++ client/src/redux/myProfileReducer.js
@@ -1,4 +1,6 @@
 const initialState = {
+    demo: true,
+    
     firstnameFieldValue: "",
     lastnameFieldValue: "",
     socialFieldValue: "",
