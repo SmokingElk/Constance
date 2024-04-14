0ced1c3bb0c9cf337b56ddc2e9a6a63da7a86e21
Smoking_Elk <andre12go@mail.ru>
2024-04-14 14:48:42 +0300

    add profile layout

--- client/src/components/MyProfile/MyProfile.jsx
+++ client/src/components/MyProfile/MyProfile.jsx
@@ -1,5 +1,6 @@
 import classes from "./MyProfile.module.css";
 import photoPlaceholder from "../../assets/imgs/teamlid_avatar.jpg";
+import Container from "../Utils/Container/Container";
 
 const MyProfile = props => {
 
@@ -24,25 +25,40 @@ const MyProfile = props => {
     };
 
     return (
-        <div>
-            <img className={classes.profile_img} src={`http://localhost:5000/static/images/${props.photoName}`} onClick={() => props.selectPhoto()}/>
+        <Container>
+            <div className={classes.layout}>
+                <div className={classes.body}>
+                    <div className={classes.title}>Profile</div>
+                    <div className={classes.columns}>
+                        <div className={classes.imgColumn}>
+                            <img className={classes.profile_img} src={`http://localhost:5000/static/images/${props.photoName}`} onClick={() => props.selectPhoto()}/>
+                        </div>
+                        
+                        <div className={classes.fieldsColumn}>
+                            <div className={classes.fieldBlock}>
+                                <div className={classes.label}>Firstname</div>
+                                <input type="text" className={"inputField"} value={props.firstnameFieldValue} placeholder="firstname" onChange={onFirstnameChange}></input>
+                            </div>
 
-            <div>
-                <textarea value={props.firstnameFieldValue} placeholder="firstname" onChange={onFirstnameChange}></textarea>
-            </div>
+                            <div className={classes.fieldBlock}>
+                                <div className={classes.label}>Lastname</div>
+                                <input type="text" className={"inputField"} value={props.lastnameFieldValue} placeholder="lastname" onChange={onLastnameChange}></input>
+                            </div>
 
-            <div>
-                <textarea value={props.lastnameFieldValue} placeholder="lastname" onChange={onLastnameChange}></textarea>
-            </div>
+                            <div className={classes.fieldBlock}>
+                                <div className={classes.label}>Social media</div>
+                                <input type="text" className={"inputField"} value={props.socialFieldValue} placeholder="social" onChange={onSocialChange}></input>
+                            </div>
 
-            <div>
-                <textarea value={props.socialFieldValue} placeholder="social" onChange={onSocialChange}></textarea>
+                            <div className={classes.fieldBlock}>
+                                <div className={classes.label}>Phone number</div>
+                                <input className={"inputField"} type="tel" pattern="+7([0-9]{3})[0-9]{3}-[0-9]{3}" placeholder="+7(999)999-99-99" value={props.phoneNumberFieldValue} onChange={onPhoneChange} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div>
-                <input type="tel" pattern="+7([0-9]{3})[0-9]{3}-[0-9]{3}" placeholder="+7(999)999-99-99" value={props.phoneNumberFieldValue} onChange={onPhoneChange} />
-            </div>
-        </div>
+        </Container>
     );
 }
 
