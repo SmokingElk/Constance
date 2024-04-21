7ffa36368dd4d80d75f7b1fbab2e620def6bf760
Smoking_Elk <andre12go@mail.ru>
2024-04-21 14:37:19 +0300

    add requests logic

--- client/src/components/Search/SearchContainer.jsx
+++ client/src/components/Search/SearchContainer.jsx
@@ -2,16 +2,31 @@ import React from "react";
 import Search from "./Search";
 import { connect } from "react-redux";
 import { addSearchPack, resetSearchResults } from "../../redux/searchReducer";
+import axios from "axios";
+import { getJWT } from "../../global_logic/userEnter.js";
+import withRouter from "../Utils/WithRouter.jsx";
 
 class SearchContainer extends React.Component {
     componentDidMount () {
         this.props.resetSearchResults();
 
-        if (this.props.demo) return;
+        this.loadNextPack(0);
     }
     
-    loadNextPack () {
+    loadNextPack (packNum = this.props.packsLoaded) {
         if (this.props.demo) return;
+
+        axios.get("http://localhost:5000/api/v1/search/get_pack", {
+            params: {
+                jwtToken: getJWT(),
+                pack_number: packNum,
+            }
+        }).then(res => {
+            this.props.addSearchPack(res.data.pack_items, res.data.is_end);
+        }).catch(error => {
+            let status = error.response.status;
+            if (status === 401) this.props.router.navigate("/login");
+        });
     }
 
     render () {
@@ -31,4 +46,4 @@ const mapDispatchToProps = {
     addSearchPack,    
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(SearchContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(SearchContainer));
