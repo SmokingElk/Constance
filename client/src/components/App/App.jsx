311d87d837d026f632752a6ee06d60ab86ebef48
Smoking_Elk <andre12go@mail.ru>
2024-05-12 09:37:04 +0300

    add privacy policy page

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,35 +1,46 @@
-import Background from '../Background/Background';
-import HeaderContainer from '../Header/HeaderContainer';
-import HomeContainer from '../Home/HomeContainer';
-import LoginContainer from '../Login/LoginContainer';
-import MyProfileContainer from '../MyProfile/MyProfileContainer';
-import Properties from '../Properties/Properties';
-import SearchContainer from '../Search/SearchContainer';
-import SignupContainer from '../Signup/SignupContainer';
-import WatchProfileContainer from '../WatchProfile/WatchProfileContainer';
-import classes from './App.module.css';
-import { Route, Routes } from 'react-router-dom';
+import Background from "../Background/Background";
+import HeaderContainer from "../Header/HeaderContainer";
+import HomeContainer from "../Home/HomeContainer";
+import LoginContainer from "../Login/LoginContainer";
+import MyProfileContainer from "../MyProfile/MyProfileContainer";
+import PrivacyPolicy from "../PrivacyPolicy/PrivacyPolicy";
+import Properties from "../Properties/Properties";
+import SearchContainer from "../Search/SearchContainer";
+import SignupContainer from "../Signup/SignupContainer";
+import WatchProfileContainer from "../WatchProfile/WatchProfileContainer";
+import classes from "./App.module.css";
+import { Route, Routes } from "react-router-dom";
 
-const App = props => {
-	return (
-		<div className={classes.app}>
-			<Background />
-			<HeaderContainer />
-			
-			<div className={classes.content}>
-				<Routes>
-					<Route path="/login" element={<LoginContainer />} />
-					<Route path="/sign_up" element={<SignupContainer />} />
-					<Route path="/properties/preferences" element={<Properties showPreferences={true}/>} />
-					<Route path="/properties/characteristics" element={<Properties showPreferences={false}/>} />
-					<Route exact path="/profile" element={<MyProfileContainer />} />
-					<Route exact path="/profile/:userId" element={<WatchProfileContainer />} />
-					<Route path="/search" element={<SearchContainer />} />
-					<Route exact path="" element={<HomeContainer />} />
-				</Routes>
-			</div>
-		</div>
-	);
-}
+const App = (props) => {
+    return (
+        <div className={classes.app}>
+            <Background />
+            <HeaderContainer />
+
+            <div className={classes.content}>
+                <Routes>
+                    <Route path="/privacy_policy" element={<PrivacyPolicy />} />
+                    <Route path="/login" element={<LoginContainer />} />
+                    <Route path="/sign_up" element={<SignupContainer />} />
+
+                    <Route
+                        path="/properties/preferences"
+                        element={<Properties showPreferences={true} />}
+                    />
+
+                    <Route
+                        path="/properties/characteristics"
+                        element={<Properties showPreferences={false} />}
+                    />
+
+                    <Route exact path="/profile" element={<MyProfileContainer />} />
+                    <Route exact path="/profile/:userId" element={<WatchProfileContainer />} />
+                    <Route path="/search" element={<SearchContainer />} />
+                    <Route exact path="" element={<HomeContainer />} />
+                </Routes>
+            </div>
+        </div>
+    );
+};
 
 export default App;
