93aec58a2763cc357e68923e5aa6ed5e946f5958
JanArsMAI <janars.2005@mail.ru>
2024-05-31 16:40:45 +0300

    adding_DOCSTRING

--- server/add_profile_image.py
+++ server/add_profile_image.py
@@ -4,6 +4,18 @@ UPLOAD_FOLDER = './static/images'
 
 
 def add_profile_image(file):
+    """
+    Функция, которая добавляет фотографию пользователя в отдельную папку static,
+    где хранятся все изображения.
+    Parameters
+    ----------
+    file : file
+        файл, который мы закидываем в папку static
+    Returns
+    -------
+    str
+        имя файла, которое будет хранится в базе данных в формате img{номер файла в папке}.png
+    """
     num_files = len([f for f in os.listdir(UPLOAD_FOLDER)
                      if os.path.isfile(os.path.join(UPLOAD_FOLDER, f))])
     file_name = f"img{num_files}.png"
